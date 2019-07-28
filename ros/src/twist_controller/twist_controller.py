e1f468b867a3652f93dc51a2276a23bc7f878e0a
pablitinho <pablogsanchez@gmail.com>
2019-07-28 19:00:33 +0200

    Fixed the CMake problem, added the Walkthrough from tutorial

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -5,7 +5,33 @@ ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
-        # TODO: Implement
+        #Controllers
+        yaw_param = {}
+        for key in ["wheel_base", "steer_ratio", "min_speed", "max_lat_accel", "max_steer_angle"]:
+            yaw_param[key] = kwargs[key]
+
+        self.yaw_controller = YawController(**yaw_param)
+        k = [x * 0.462 for x in [1, 0.08, 0.006]]  # PID coefficients
+        output_range = [kwargs["decel_limit"], kwargs["accel_limit"]]
+        pid_param = k + output_range
+        self.throttle_controller = PID(*pid_param)
+
+        # Low pass filters
+        self.ts = 1. / (kwargs["DBW_FREQ"])  # Sampling period
+        self.tau = 2 * self.ts  # Cutoff frequency
+        self.vel_lpf = LowPassFilter(self.tau, self.ts)
+
+        # Vehicle parameters
+        self.vehicle_mass = kwargs["vehicle_mass"]
+        self.fuel_capacity = kwargs["fuel_capacity"]
+        self.brake_deadband = kwargs["brake_deadband"]
+        self.decel_limit = kwargs["decel_limit"]
+        self.accel_limit = kwargs["accel_limit"]
+        self.wheel_radius = kwargs["wheel_radius"]
+        self.min_speed = kwargs["min_speed"]
+
+        # Timestamp
+        self.last_time = rospy.get_time()
         pass
 
     def control(self, *args, **kwargs):
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,9 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from scipy.spatial import KDTree
+import numpy as np
+from std_msgs.msg import Int32
 
 import math
 
@@ -22,7 +25,9 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+MAX_DECEL = 1.0
+MPS2MPH = 2.236936
+SAFETY_FACTOR = 0.90
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -37,19 +42,47 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+        self.stopline_wp_idx = -1
+        self.speed_limit = rospy.get_param('/waypoint_loader/velocity') / 3.6
+        rospy.loginfo("Speed limit set to %.2f MPH", self.speed_limit * MPS2MPH)
 
-        rospy.spin()
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+
+        #rospy.spin()
+
+        self.loop()
+
+    def loop(self):
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints and self.waypoint_tree:
+                self.publish_waypoints()
+            rate.sleep()
 
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        self.pose = msg
+        #pass
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[w.pose.pose.position.x, w.pose.pose.position.y] for w in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
+
         pass
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
+        self.stopline_wp_idx = msg.data
         pass
 
     def obstacle_cb(self, msg):
@@ -70,7 +103,61 @@ class WaypointUpdater(object):
             wp1 = i
         return dist
 
+    def generate_lane(self):
+        lane = Lane()
 
+        closest_idx = self.get_closest_waypoint_id()
+        farthest_idx = closest_idx + LOOKAHEAD_WPS
+        waypoints = self.base_waypoints.waypoints[closest_idx:farthest_idx]
+
+        if self.stopline_wp_idx == -1 or (self.stopline_wp_idx >= farthest_idx):
+            lane.waypoints = waypoints
+        else:
+            lane.waypoints = self.decelerate_waypoints(waypoints, closest_idx)
+
+        return lane
+
+    def publish_waypoints(self):
+        if not self.base_waypoints:
+            return
+        lane = self.generate_lane()
+        self.final_waypoints_pub.publish(lane)
+
+    def get_closest_waypoint_id(self):
+        """
+        Returns the index of the closest waypoint ahead of the vehicle
+        """
+        if self.waypoint_tree:
+            pt = [self.pose.pose.position.x, self.pose.pose.position.y]
+            closest_id = self.waypoint_tree.query(pt, 1)[1]
+
+            closest_pt = np.array(self.waypoints_2d[closest_id])
+            prev_pt = np.array(self.waypoints_2d[closest_id - 1])
+            pt = np.array(pt)
+            value = np.dot(closest_pt - prev_pt, pt - closest_pt)
+            if value > 0:
+                closest_id = (closest_id + 1) % len(self.waypoints_2d)
+
+            return closest_id
+        return 0
+
+    def decelerate_waypoints(self, waypoints, closest_idx):
+        result = []
+        for i, wp in enumerate(waypoints):
+            new_point = Waypoint()
+            new_point.pose = wp.pose
+
+            stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0)  # Two waypints back from line so the front of
+            # the car stops at the line
+            dist = self.distance(waypoints, i, stop_idx)
+            vel = math.sqrt(2 * MAX_DECEL * SAFETY_FACTOR * dist)
+            if vel < 1.0:
+                vel = 0.0
+
+            new_point.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
+            result.append(new_point)
+
+        return result
 if __name__ == '__main__':
     try:
         WaypointUpdater()
